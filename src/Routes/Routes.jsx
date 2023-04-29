e611e4931e76d4130b5c6d6cff23a2e5f5f92a39
soikotdeb <sdsoikot424@gmail.com>
2023-04-29 20:27:26 +0600

    added new

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -43,7 +43,7 @@ element:<Navigate to="/category/0"></Navigate>
             {
                 path:':id',
                 element:<Category></Category>,
-                loader:({params})=>fetch(`http://localhost:5000/categories/${params.id}`)
+                loader:({params})=>fetch(`https://the-news-dragon-server-soikotdeb.vercel.app/categories/${params.id}`)
             }
          
         ]
@@ -55,7 +55,7 @@ element:<Navigate to="/category/0"></Navigate>
             {
                 path:':id',
                 element:<PrivateRoute><News></News></PrivateRoute>,
-                loader:({params})=>fetch(`http://localhost:5000/news/${params.id}`)
+                loader:({params})=>fetch(`https://the-news-dragon-server-soikotdeb.vercel.app/news/${params.id}`)
             
             }
         ]
